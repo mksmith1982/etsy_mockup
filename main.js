a4300ed7e522030cb1c7d047e778908af1746bac
mksmith1982 <mksmith1982@gmail.com>
2016-10-10 16:16:00 -0400

    modified js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 
-//question 1// The average prices of the items//
+function solution1 (items) {
+
+  var avgPrice = items.map(function (items) {
+    return items.price;
+  });
+
+  var sum = avgPrice.reduce(function (total, nextPrice) {
+    return total + nextPrice;
+  });
+
+  var average = sum / avgPrice.length;
+
+  var answer1 = document.getElementById("answer1");
+  answer1.innerHTML = "The average price is: $" + average.toFixed(2);
+}
+
+
+
+
+
+
+
  var avgPrice = items.map(function (items) {
    return items.price;
  });
@@ -14,38 +35,90 @@
 //avgPrice(items);
 
 //Show me how to get an array of items that cost between $14.00 and $18.00 USD//
+//I need to get a new array for price.... so I take items[0]-[24].price
+
+
+// var filterPrice = prices.filter(prices)}
+// var rng= (prices > 14 && prices < 18)
+// return rng;
+// myArr.filter(function(singleItem){
+// 	return singleItem > 14 && singleItem < 18;
+// });
+
+function midPriceItems (items) {
+  var rng = items.filter(function(items) {
+	return items.price > 14 && items.price < 18;
+	})
+	return rng;
+  };
 
 
 
-  
-var prices = [];
-    price = items.price;{
-for(var i in items)
-if (price > 14 && price <18); 
+var answer2 = midPriceItems(items);
+var answer2Box = document.getElementById("answer2");
 
-  answer2.innerHTML = "Priced Between :$";
-  
 
-document.getElementById("answer2");}
+for (var i = 0; i < answer2.length; i++) {
+
+  answer2Box.innerHTML += "Price Range:" + answer2[i].title + "<br>";
+}
+
+
+
+
 
 
 
 
 //Which item has a "GBP" currency code? Display it's name and price.
+var gbp=items.filter(function(item){
+return item.currency_code !== "USD";
+
+});
+var answer3 = gbp
+var answer3Box = document.getElementById("answer3");
+for (var i = 0; i < answer3.length; i++) {
+	
+answer3Box.innerHTML += answer3[i].title; + "<br>"
+answer3Box.innerHTML += answer3[i].price; + "<br>"
+answer3Box.innerHTML += answer3[i].currency_code;
+
+
+}
+
+// // //Display a list of all items who are made of wood.
+
+
+// var woodItems = items.filter(function(items) {
+// 	return items.materials === "wood";
+//});
+// var answer4
+
+///.includes   
+
+/// loop over each object 
 
 
 
-document.getElementById("answer3");}
-
-//Display a list of all items who are made of wood.
-
-document.getElementById("answer4");}
-//Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
-
-document.getElementById("answer5");}
 
 
-//How many items were made by their sellers?
 
 
-document.getElementById("answer6");}
+
+
+
+
+// document.getElementById("answer4");}///  FILTER//
+// //Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+
+// document.getElementById("answer5");}
+
+
+// //How many items were made by their sellers?
+
+
+// document.getElementById("answer6");}
+
+// for (var i = 0; i < items.length; i++) {
+// 	items[i]; return price;
+// }
